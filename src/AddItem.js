06aeef9d0aa3881966a7babc89d1d91bfbf09917
Tien Borland <nguy2819@crk.umn.edu>
2018-09-20 20:48:58 -0600

    removeItemFromCart with logic: cartItem.count -= cartItem.count > 0 ? 1 : 0;

--- src/AddItem.js
+++ src/AddItem.js
@@ -1,8 +1,4 @@
-import React, { Component } from 'react';
-
-
-
-        
+import React, { Component } from 'react';    
 
 export default class AddItem extends React.Component{
     constructor(props){
@@ -12,6 +8,8 @@ export default class AddItem extends React.Component{
         }
 
         this.addItemToCart = this.addItemToCart.bind(this);
+        this.removeItemFromCart = this.removeItemFromCart.bind(this);
+
     }
 
     addItemToCart(name, price, count){
@@ -30,12 +28,27 @@ export default class AddItem extends React.Component{
         }
     }
 
+    removeItemFromCart(name, price, count){
+        if(this.state.cart.some(item=> item.name === name)){
+          this.setState({cart: this.state.cart.map(
+            cartItem => {
+              if(cartItem.name === name){
+                cartItem.count -= cartItem.count > 0 ? 1 : 0;
+              }
+              return cartItem;
+            })
+          });
+        }
+      }
+
     render(){
         return (
         <div>
             <h2> Products </h2>
             <button onClick={() => this.addItemToCart('apple', 5, 1)}>Add Apple</button>
+            <button onClick={() => this.removeItemFromCart('apple', 5, 1)}>Remove Apple</button>
             <button onClick={() => this.addItemToCart('orange', 10, 1)}>Add orange</button>
+            <button onClick={() => this.removeItemFromCart('orange', 5, 1)}>Remove Orange</button>
             <div>
                 <h2>Cart</h2>
                 <ul>
